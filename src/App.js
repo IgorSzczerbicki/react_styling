de9517d4c8fdc363076ed1ca056245fbfba79aec
Igor Szczerbicki <pl4cek@outlook.com>
2018-07-09 16:00:24 +0200

    Dodawanie i wykorzystywanie Radium - pseudoklasy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person'
+import Radium from 'radium'
 
 class App extends Component {
 
@@ -49,7 +50,11 @@ class App extends Component {
 			font: 'inherit',
 			border: '1px solid blue',
 			padding: '8px',
-			cursor: 'pointer'
+			cursor: 'pointer',
+			':hover': {
+				backgroundColor: 'lightgreen',
+				color: 'black'
+			}
 		};
 
 		let persons = null;
@@ -68,6 +73,10 @@ class App extends Component {
 				</div>
 			);
 			btnStyle.backgroundColor = 'red';
+			btnStyle[':hover'] = {
+				backgroundColor: 'salmon',
+				color: 'black'
+			}
 		}
 
 		let classes = [];
@@ -92,4 +101,4 @@ class App extends Component {
 	}
 }
 
-export default App;
+export default Radium(App);
